a60105b8cdfe14f2a1d02a880dbb31a3515f0e40
louischiang <mingyangchiang@163.com>
2018-08-24 01:59:08 +0800

    完善热歌榜

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 $(function(){
 	$.get('./songs.json').then(function(response){
-		console.log(response)
-		console.log('23')
+		$('.tabContents>li').eq(0).addClass('active')
 		let items = response;
 		items.forEach((e)=>{
-			console.log('1111')
-			console.log(e)
 			let $li = $(`
 				<li>
 					<a href="./song.html?id=${e.id}">
@@ -21,10 +18,65 @@ $(function(){
 			`)
 			$('#latestSongs').append($li)
 		})
-		$('.loading').remove()
-	}, function(){
-		alert('failed!...')
+		$('.latestMusic>.loading, .tabContents>.loading').remove()
+		//$('.tabContents>.tab1').remove()
+
+	},()=>{alert('failed...')})
+
+    //tab切换功能
+	$('.siteNav').on('click', '.tabItems>li', function(e){
+		let $target = $(e.currentTarget)
+		let liIndex = $target.index()
+		console.log(liIndex)
+		$target.addClass('active').siblings().removeClass('active')
+		$target.trigger('onChange', liIndex)
+		$('.tabContents>li').eq(liIndex).addClass('active').siblings().removeClass('active')
 	})
+
+	$('.siteNav').on('onChange', function(e, liIndex){
+	    let $li = $('.tabContents>li').eq(liIndex)
+	    if($li.attr('contentLoaded') === 'yes'){return}
+		if( liIndex === 1 ){
+			$.get('./hotsongs.json').then(function(response){
+				$li.attr('contentLoaded', 'yes')
+				let today = new Date()
+				let day = today.getDate()
+				let month = today.getMonth() + 1
+				$('.hot-music-date').text(`更新日期：${pad(month)}月${day}日`)
+
+				$('.tab2>.loading').remove()
+			}, ()=>{alert('failed...')}).then(function(){
+				$.get('./songs.json').then(function(response){
+					let items = response;
+					items.forEach((e, i)=>{
+						let $li = $(`
+							<li class="hot-music-li">
+								<a id="hot-music-item" href="./song.html?id=${e.id}">
+									<div class="rank">${pad(i+1)}</div>
+									<div class="hot-music-info">
+										<h3>${e.name}</h3>
+									    <p>${e.singer}-${e.albumn}</p>
+								        <svg class="icon play-circled" aria-hidden="true">
+						                  <use xlink:href="#icon-play-circle-big"></use>
+							            </svg>
+									</div>
+						        </a>
+					        </li>
+						`)
+						$('#tab2List').append($li)
+					})
+				},()=>{alert('failed...')})
+			})
+		}
+		if( liIndex === 2 ){
+			console.log('search')
+			$('.tab3>.loading').remove()
+		}
+	})
+
+	function pad(number){
+		return number >= 10 ? number + '' : '0' + number
+	}
 })
 
 
